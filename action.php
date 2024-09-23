39b49cfdc49b972b8e4b6dcb1bd930bb37ab2599
Camberden <42500247+Camberden@users.noreply.github.com>
2024-09-23 09:28:35 -0400

    Toying around

---
+++ action.php
@@ -0,0 +1,32 @@
+<?php
+
+// NOTES:
+// Testing Server >>>>> php -S localhost:8000
+// Close Server >>>>> "Control C"
+// Open Command Line >>>>> "Command J"
+// Clear Command Line >>>>> "Command K"
+// Check Version >>>>> php --version
+// Composer is PHP's NPM
+// Import Dependencies >>>>> require, use.
+// Make folder, load into VS Code, create repo in-app, git add . , git commit -m "", 
+// THEN, configure push: git remote add origin https://github.com/Camberden/namenamename.git
+// FINALLY, git push --set-upstream origin master, DONE
+//
+// 
+
+// This Slugify dependency basically just turns stuff into Slug (kebab) case!
+require __DIR__ . "/vendor/autoload.php";
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify("The sky is green and the grass is blue!");
+
+$niftyShit = 2 + 2;
+$name = "Chrispy";
+echo $niftyShit; // SIGIL VARIABLE WORKS NICELY WITHIN DOUBLE QUOTES
+echo "My name is $name and I have " . touchTheMoon($niftyShit) . " instances of nifty shit (incense).";
+
+function touchTheMoon($x){
+	return $x * 2;
+}
+?>
--- index.php
+++ index.php
@@ -1,26 +1,94 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-US">
+<!-- Hi there! :V -->
 
-// NOTES:
-// Testing Server >>>>> php -S localhost:8000
-// Close Server >>>>> "Control C"
-// Open Command Line >>>>> "Command J"
-// Clear Command Line >>>>> "Command K"
-// Check Version >>>>> php --version
-// Composer is PHP's NPM
-// Import Dependencies >>>>> require, use.
+<head>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="assets/image-crystal-chrispy-favicon.png">
+	<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+	<link href="https://fonts.googleapis.com/css?family=Lora:400italic" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker:400" rel="stylesheet" type="text/css">
+	<title>Chrispy's PHP Playground</title>
+</head>
 
-// This Slugify dependency basically just turns stuff into Slug (kebab) case!
-require __DIR__ . "/vendor/autoload.php";
-use Cocur\Slugify\Slugify;
+<body>
+	
+	<main class="index-grid">
 
-$slugify = new Slugify();
-echo $slugify->slugify("The sky is green and the grass is blue!");
+		<h1 id="camberden">camberden</h1>
 
-$niftyShit = 2 + 2;
-$name = "Chrispy";
-echo $niftyShit; // SIGIL VARIABLE WORKS NICELY WITHIN DOUBLE QUOTES
-echo "My name is $name and I have " . touchTheMoon($niftyShit) . " instances of nifty shit (incense).";
+		<div class="index-item site-info">
+			<p>	
+				Welcome to camberden's php stuff.<br>
+				Here you will find practice.<br>
+				Nothing but fun practice.<br>
+				An expression of my being! I guess.
+			</p>
+		</div>
 
-function touchTheMoon($x){
-	return $x * 2;
-}
+		<div class="plaything">
+			<h2>
+				<?php 
+					echo "Hello, you.<br>";
+					echo "Echoed from the depths.";
+					
+					$yoshis = [
+						"Red",
+						"Blue",
+						"Yellow",
+						"Light Blue",
+						"White",
+						"Orange",
+						"Pink",
+					];
+
+					$containers = [
+						[
+							"Crate" => "Apple",
+							"Bottle" => "Water",
+						],
+						[
+							"Bin" => "Banana",
+							"Bucket" => "Juice",
+						],
+					];
+
+					function filterYoshis($yoshis) {
+						return $yoshis[0];
+					}
+
+				?>
+
+			</h2>
+
+			<ul>Yoshis<hr>
+				<?php foreach ($yoshis as $yoshi) {
+					echo "<li>Hi</li>";
+				};	
+				?>
+				<hr>
+				<?php foreach ($yoshis as $yoshi): ?>
+				<li><?=$yoshi?></li>
+				<?php endforeach; ?>
+				<li> <?= filterYoshis($yoshis); ?> </li>
+
+			</ul>
+
+
+		</div>
+
+
+
+
+		<div class="index-item footer">
+			<p class="marker" id="footer-text">Made with ❤️ by Chrispy</span></p>
+		</div>
+
+	</main>
+
+	
+
+	<!-- <script src="action.js"></script> -->
+</body>
+
+</html>
